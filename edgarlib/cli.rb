51cc3016e5b14054692ce521c768e23fafff811c
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2021-06-01 21:09:09 +0900

    Fix: 変数名を修正。

--- edgarlib/cli.rb
+++ edgarlib/cli.rb
@@ -5,7 +5,7 @@ require 'optionparser'
 module Edgarlib
   module CliTools
     class Args
-      attr_accessor :ticker_cid, :install_xbrl
+      attr_accessor :t, :i
 
       public
       def initialize
@@ -15,14 +15,14 @@ module Edgarlib
       end
 
       def to_s
-        "ticker_cid: #{self.ticker_cid} install_xbrl: #{self.install_xbrl}"
+        "-T: #{self.t} -I: #{self.i}"
       end
 
       private
       def parse_args
         option_parser = OptionParser.new
-        option_parser.on('-T') {|v| self.ticker_cid = true}
-        option_parser.on('-I') {|v| self.install_xbrl = true}
+        option_parser.on('-T') {|v| self.t = true}
+        option_parser.on('-I') {|v| self.i = true}
         option_parser.parse(ARGV)
       end
     end
