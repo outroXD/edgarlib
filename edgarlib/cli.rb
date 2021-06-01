987988c96722f7bef91fbfe01e1df8684600202d
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2021-06-01 13:42:08 +0900

    Add: ロガー追加。

--- edgarlib/cli.rb
+++ edgarlib/cli.rb
@@ -4,13 +4,27 @@ require 'optionparser'
 
 module Edgarlib
   module CliTools
-    def self.parse_args
-      options = Hash.new(false)
-      option_parser = OptionParser.new
-      option_parser.on('-T') {|v| options[:T] = true}
-      option_parser.on('-I') {|v| options[:I] = true}
-      option_parser.parse(ARGV)
-      options
+    class Args
+      attr_accessor :ticker_cid, :install_xbrl
+
+      public
+      def initialize
+        super
+        self.parse_args
+        LOGGER.info(self.to_s)
+      end
+
+      def to_s
+        "ticker_cid: #{self.ticker_cid} install_xbrl: #{self.install_xbrl}"
+      end
+
+      private
+      def parse_args
+        option_parser = OptionParser.new
+        option_parser.on('-T') {|v| self.ticker_cid = true}
+        option_parser.on('-I') {|v| self.install_xbrl = true}
+        option_parser.parse(ARGV)
+      end
     end
   end
 end
--- edgarlib/main.rb
+++ edgarlib/main.rb
@@ -1,11 +1,15 @@
 # encoding: utf-8
 
+require 'logger'
 require_relative 'cli'
 
 module Edgarlib
+  LOGGER = Logger.new('../outputs/logs/edgarlib.log')
+
   class Main
     def self.run
-      args = Edgarlib::CliTools::parse_args
+      args = Edgarlib::CliTools::Args.new
+      print "STOP"
     end
   end
 end
