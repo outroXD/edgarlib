dd3e910c4aa7b88367a2bd1787237b95dc9bf005
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2021-06-05 21:50:40 +0900

    Add: 企業検索用URL生成処理を追加。

--- edgarlib/secgov.rb
+++ edgarlib/secgov.rb
@@ -3,6 +3,7 @@
 require 'net/http'
 require 'uri'
 require 'json'
+require 'singleton'
 require_relative 'company'
 
 module Edgarlib
@@ -38,6 +39,84 @@ module Edgarlib
           end
           @ticker_cik_cache
         end
+      end
+    end
+
+    class CompanySearchUrlBuilder
+      module CompanySearchUrlStatus
+        module OwnerStatus
+          INCLUDE = 'include'.freeze
+        end
+        module OutputStatus
+          ATOM = 'atom'.freeze
+        end
+        module ActionStatus
+          GET_COMPANY = 'getcompany'.freeze
+        end
+      end
+
+      @@base_url = 'https://www.sec.gov/cgi-bin/browse-edgar?'
+
+      def initialize(cik, action=nil, type=nil, dateb=nil, owner=nil, start=nil, count=nil, output=nil)
+        @cik = cik
+
+        if action.nil?
+          @action = CompanySearchUrlStatus::ActionStatus::GET_COMPANY
+        else
+          @action = action
+        end
+
+        @type = type
+        @dateb = dateb
+
+        if owner.nil?
+          @owner = CompanySearchUrlStatus::OwnerStatus::INCLUDE
+        else
+          @owner = owner
+        end
+
+        if start.nil?
+          @start = 0
+        else
+          @start = start
+        end
+
+        if count.nil?
+          @count = 100
+        else
+          @count = count
+        end
+
+        if output.nil?
+          @output = CompanySearchUrlStatus::OutputStatus::ATOM
+        else
+          @output = output
+        end
+      end
+
+      public
+      def get_url
+        url = @@base_url
+        url << "action=#{@action}"
+        url << "&CIK=#{@cik}"
+
+        if @type.nil?
+          url << "&type="
+        else
+          url << "&type=#{@type}"
+        end
+
+        if @dateb.nil?
+          url << "&dateb="
+        else
+          utl << "$dateb=#{@dateb}"
+        end
+
+        url << "&owner=#{@owner}"
+        url << "&start=#{@start}"
+        url << "&count=#{@count}"
+        url << "&output=#{@output}"
+        url
       end
     end
   end
--- tests/secgov_spec.rb
+++ tests/secgov_spec.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require_relative '../edgarlib/secgov'
 
 describe "secgov" do
-  it 'tickerを指定して取得したデータから、cik/ticker/titleを取得・確認するケース' do
+  it '正常 tickerを指定して取得したデータから、cik/ticker/titleを取得・確認するケース' do
     cik = 320193
     ticker = "AAPL"
     title = "Apple Inc."
@@ -15,10 +15,26 @@ describe "secgov" do
     expect(title).to eq company.title
   end
 
-  it 'tickerとして存在しない値を渡してcik/ticker/titleの取得を試みたケース' do
+  it '正常 tickerとして存在しない値を渡してcik/ticker/titleの取得を試みたケース' do
     ticker = "DUMMY"
     company = Edgarlib::SecGov::TickerCik.new.get_company_by_ticker(ticker)
 
     expect(nil).to eq company
+  end
+
+  it '正常 企業検索URL生成処理' do
+    cik = 320193
+    action = nil
+    type = nil
+    dateb = nil
+    owner = nil
+    start = nil
+    count = nil
+    output = nil
+    url = Edgarlib::SecGov::CompanySearchUrlBuilder.new(
+      cik=cik, action=action, type=type, dateb=dateb, owner=owner, start=start, count=count,output=output).get_url
+
+    expect_url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=320193&type=&dateb=&owner=include&start=0&count=100&output=atom"
+    expect(expect_url).to eq url
   end
 end
