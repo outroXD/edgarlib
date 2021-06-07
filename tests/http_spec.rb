e7c008e9981f27b21c0c0f1c9d28bf0d08cad3d0
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2021-06-07 21:22:40 +0900

    Add: テスト追加。

--- tests/http_spec.rb
+++ tests/http_spec.rb
@@ -9,6 +9,5 @@ describe "http" do
     cik = 320193
     url = Edgarlib::SecGov::CompanySearchUrlBuilder.new(cik=cik).get_url
     xml = Edgarlib::Http.instance.get_response_as_xml(url=url)
-    print "STOP"
   end
 end
