c5815646e56f55f7a8474960e9b113536d491d70
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2021-06-07 20:40:11 +0900

    Add: --installが引数を取れるように修正。

--- edgarlib/cli.rb
+++ edgarlib/cli.rb
@@ -5,11 +5,11 @@ require 'optionparser'
 module Edgarlib
   module CliTools
     class Args
-      attr_accessor :t, :i
+      attr_accessor :t, :i, :tickers
 
       public
       def initialize
-        super
+        self.tickers = []
         self.parse_args
         LOGGER.info(self.to_s)
       end
@@ -22,7 +22,10 @@ module Edgarlib
       def parse_args
         option_parser = OptionParser.new
         option_parser.on('-T') {|v| self.t = true}
-        option_parser.on('-I') {|v| self.i = true}
+        option_parser.on('-I', '--install <ticker>', 'tickerで指定したXBRLをローカルにダウンロードする。') do |v|
+          self.i = true
+          self.tickers.append(v.upcase)
+        end
         option_parser.parse(ARGV)
       end
     end
